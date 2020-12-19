3836946b9c84f7ffcc35d5257e1b47e122aac990
BoJoNVi <bonjomontes@gmail.com>
2020-12-19 18:33:03 +0800

    Testing

--- app/src/main/java/com/example/budgetmanager/MainActivity.kt
+++ app/src/main/java/com/example/budgetmanager/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.budgetmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"asdasdasdasd", Toast.LENGTH_LONG).show()
     }
 }
